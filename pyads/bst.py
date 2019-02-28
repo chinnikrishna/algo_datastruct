6f47deea1216a5d7a3e0a235e98fb6f2643c5b98
chinnikrishna.kothapalli <ckothapa@nervana-titanxp21.fm.intel.com>
2019-02-27 21:16:07 -0800

    Mid work

--- pyads/bst.py
+++ pyads/bst.py
@@ -3,21 +3,56 @@ class BTNode:
 		self.key = data
 		self.left = None
 		self.right = None
+		self.parent = None
 
 class BinarySearchTree:
-	def __init__(self, rootVal):
+	def __init__(self):
 		self.root = None
 	
 	def insNode(self, data):
-		n = BTNode(data):
+		# Create a new node
+		n = BTNode(data)
+		# If tree is empty create the first one
 		if self.root == None:
-			if(data < self.root):
-				self.root.left = n
-			else:
-				self.root.right = n
-		else:
 			self.root = n
-		
+		else:
+			# Get the root
+			curr = self.root
+			# Iterate until you reach the right leaf node
+			while curr is not None:
+				# Keep track present parent
+				parent = curr
+				# Go left if less
+				if data < curr.key:
+					curr = curr.left
+				# Go right if greater
+				else:
+					curr = curr.right
+			# Now insert
+			if data < parent.key:
+				parent.left = n
+			else:
+				parent.right = n
+			n.parent = parent
+	
+	
+	def preOrder(self, root):
+		if root:
+			print(root.key)
+			self.preOrder(root.left)
+			self.preOrder(root.right)
+			
+
+	
+
+bst = BinarySearchTree()
+bst.insNode(10)
+bst.insNode(67)
+bst.insNode(42)
+bst.insNode(23)
+bst.insNode(89)
+bst.insNode(34)
+bst.preOrder(bst.root)		
 	
 
 
---
+++ pyads/graph.py
@@ -0,0 +1,23 @@
+class Graph:
+
+	def __init__(self):
+		pass
+	
+	def addVert(self, vert):
+		pass
+		
+	def addEdge(self, fromV, toV):
+		pass
+		
+	def addEdge(self, fromV, toV, weight):
+		pass
+	
+	def getVertex(self, vertKey):
+		pass
+	
+	def getVertices(self):
+		pass
+	
+	def __in__(self):
+		pass
+		
